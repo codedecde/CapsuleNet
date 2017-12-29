a44457c16ca62ba6640a08d3b1a57f91ff010b89
codedecde <barunpatra95@gmail.com>
2017-12-30 02:24:27 +0530

    debugging needed

---
+++ capsule.py
@@ -0,0 +1,112 @@
+import torch
+import pdb
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+
+class CapsuleNet(nn.Module):
+    def __init__(self, **kwargs):
+        super(CapsuleNet, self).__init__()
+        inp_h, inp_w = 28, 28
+        # Conv 1 info
+        self.conv1_s = 1
+        self.conv1_c = 256
+        self.conv1_f = 9
+        self.conv1 = nn.Conv2d(1, self.conv1_c, self.conv1_f, stride=self.conv1_s)
+
+        conv_h, conv_w = self.get_conv_dim(inp_h, self.conv1_f, self.conv1_s), self.get_conv_dim(inp_w, self.conv1_f, self.conv1_s)
+        # PrimaryCaps info
+        self.pcaps_n = 32
+        self.pcaps_s = 2
+        self.pcaps_d = 8
+        self.pcaps_f = 9
+
+        pcaps_h, pcaps_w = self.get_conv_dim(conv_h, self.pcaps_f, self.pcaps_s), self.get_conv_dim(conv_w, self.pcaps_f, self.pcaps_s)
+        for i in xrange(self.pcaps_n):
+            setattr(self, 'pcaps_conv_%d' % (i), nn.Conv2d(self.conv1_c, self.pcaps_d, self.pcaps_f, stride=self.pcaps_s))
+        # DigiCaps Layer
+        self.dcaps_n = 10
+        self.dcaps_d = 16
+        self.n_iter = 2
+
+        self.W = nn.Parameter(torch.Tensor(np.random.normal(0, 0.01, (self.dcaps_n,
+                                           pcaps_h * pcaps_w * self.pcaps_n, self.pcaps_d, self.dcaps_d))))
+        self.register_parameter('W_ij', self.W)
+        self.reconstruction_dims = [('relu', 512), ('relu', 1024), ('sigmoid', 784)]
+        in_dim = self.dcaps_d
+        for ix, (activation, out_dim) in enumerate(self.reconstruction_dims):
+            layer_name = 'reconstruction_%d' % (ix)
+            layer = nn.Linear(in_dim, out_dim)
+            setattr(self, layer_name, layer)
+            in_dim = out_dim
+
+    def get_conv_dim(self, l, fs, s, p=0):
+        """
+        Gets dimension after a filter application.
+            :param l: Length along the dimension
+            :param fs: Filter size
+            :param s: Stride
+            :param p: Padding
+        """
+        return ((l + 2 * p - fs) // s) + 1
+
+    def squash(self, t, axis):
+        """
+        Performs the squashing operation
+            :param t: Tensor to squash
+            :param axis: axis along which to squash
+        """
+        norm_t_sq = (t * t).sum(axis, keepdim=True)
+        norm_t = torch.sqrt(norm_t_sq)
+        ret_t = norm_t_sq / (1. + norm_t_sq) * (t / norm_t)
+        return ret_t
+
+    def forward(self, inp):
+        """
+        The forward pass
+            :param inp: batch x 1 x 28 x 28: The input image
+            :return v: batch x 10 x 16: The digicaps layer
+        """
+        conv1 = F.relu(self.conv1(inp))  # batch x 256 x 20 x 20
+        caps = []
+        for i in xrange(self.pcaps_n):
+            caps_val = getattr(self, 'pcaps_conv_%d' % (i))(conv1)  # batch x 8 x 6 x 6
+            caps_val = self.squash(caps_val, 1)  # batch x 8 x 6 x 6
+            caps.append(caps_val.unsqueeze(1))
+        caps = torch.cat(caps, 1)  # batch x 32 x 8 x 6 x 6
+
+        caps = caps.transpose(2, -1)  # batch x 32 x 6 x 6 x 8
+        caps = caps.contiguous().view(caps.size(0), -1, caps.size(-1))  # batch x 1152 x 8
+
+        # Now the DigiCaps Layer
+        caps_prime = caps.unsqueeze(1).expand(-1, self.dcaps_n, -1, -1).contiguous().view(-1, caps.size(-1)).unsqueeze(1)  # batch * 10 * 1152 x 1 x 8
+        w_prime = self.W.unsqueeze(0).expand(caps.size(0), -1, -1, -1, -1).contiguous().view(-1, self.W.size(-2), self.W.size(-1))  # batch * 10 * 1152 x 8 x 16
+        u_hat = torch.bmm(caps_prime, w_prime).squeeze(1).view(caps.size(0), self.dcaps_n, caps.size(1), -1)  # batch x 10 x 1156 x 16
+        b = Variable(torch.zeros((caps.size(0), self.dcaps_n, caps.size(1))))
+        if torch.cuda.is_available():
+            b = b.cuda()
+        # Setting up routing
+        for i in xrange(self.n_iter):
+            c = F.softmax(b, dim=-1)
+            s = (c.unsqueeze(-1) * u_hat).sum(2)
+            v = self.squash(s, -1)  # batch x 10 x 16
+            a = (u_hat * v.unsqueeze(2)).sum(-1)
+            b = b + a
+        return v
+
+    def reconstruct(self, digicaps, gold_labels):
+        """
+        Reconstructs the image based on the gold label
+            :param digicaps: batch x 10 x 16: The digicaps layer
+            :param gold_labels: batch,: The gold labels (Torch Variable)
+            :return masked_v: batch x 784: The image
+        """
+        idx = gold_labels.unsqueeze(1).expand(-1, digicaps.size(-1)).unsqueeze(1)
+        masked_v = torch.gather(digicaps, 1, idx).squeeze(1)
+        # self.reconstruction_dims = [('relu', 512), ('relu', 1024), ('sigmoid', 784)]
+        for ix, (activation, _) in enumerate(self.reconstruction_dims):
+            layer_name = 'reconstruction_%d' % (ix)
+            masked_v = getattr(F, activation)(getattr(self, layer_name)(masked_v))
+        return masked_v
---
+++ run-capsule.py
@@ -0,0 +1,100 @@
+import scipy.io as sio
+import numpy as np
+from capsule import CapsuleNet
+from torch.autograd import Variable
+import torch.optim as optim
+import pdb
+import torch
+
+# ========= Set Seed for replication ==== #
+np.random.seed(42)
+# ========= LOAD MNIST DATA ============= #
+mnist = sio.loadmat('Data/mnist-original.mat')
+data = mnist['data'].transpose()
+labels = mnist['label'].transpose()
+index = range(data.shape[0])
+np.random.shuffle(index)
+data = data[index]
+labels = labels[index].flatten()
+
+VAL_FRAC = 0.9
+train_x, train_y = data[:int(VAL_FRAC * data.shape[0])], labels[:int(VAL_FRAC * data.shape[0])]
+val_x, val_y = data[int(VAL_FRAC * data.shape[0]):], labels[int(VAL_FRAC * data.shape[0]):]
+
+
+def get_onehot(tensor, labels=10):
+    """
+    Converts tensor to onehot tensor
+        :param tensor: batch, : The indices of labels
+        :param labels: int: Number of labels
+        :returns one_hot: batch x labels: One hot encoding for the labels
+    """
+    one_hot = np.zeros((tensor.shape[0], labels))
+    one_hot[range(tensor.shape[0]), tensor.astype(int)] = 1.
+    return one_hot
+
+
+def margin_loss(digicaps, labels_onehot):
+    def margin_mask(tensor):
+        """
+        computes max(0, tensor_val)
+        """
+        mask = torch.gt(tensor, 0).float()
+        return tensor * mask
+    """
+    The margin loss, as defined in the paper.
+        :param digicaps: batch x 10 x 16
+        :param labels: batch x num_labels
+    """
+    norm_vec = torch.sqrt((digicaps ** 2).sum(-1))
+    m_pos = 0.9
+    m_neg = 0.1
+    lbda = 0.5
+    # pdb.set_trace()
+    m1 = margin_mask(m_pos - norm_vec)
+    m2 = margin_mask(norm_vec - m_neg)
+    mloss = ((labels_onehot * m1) + (lbda * (1. - labels_onehot) * m2)).sum(-1)
+    mloss = mloss.sum(-1).mean()
+    return mloss
+
+
+def reconstruction_loss(reconstruction, orig):
+    """
+    Computes the reconstruction loss
+        :param reconstruction: batch x im_size
+        :param orig: batch x im_size
+        :return rloss: The reconstruction loss
+    """
+    rloss = ((reconstruction - orig) ** 2).sum(-1)
+    rloss = rloss.mean()
+    return rloss
+
+
+# ========= CapsuleNet ================== #
+model = CapsuleNet()
+opt = optim.Adam(model.parameters(), lr=0.01)
+# ========= TrainingLoop ================ #
+NEPOCHS = 20
+BATCH_SIZE = 16
+for epoch in xrange(NEPOCHS):
+    index = range(train_x.shape[0])
+    np.random.shuffle(index)
+    train_x, train_y = train_x[index], train_y[index]
+    steps = -(- train_x.shape[0] // BATCH_SIZE)
+    for step, ix in enumerate(xrange(0, train_x.shape[0], BATCH_SIZE)):
+        batch_x = train_x[ix: ix + BATCH_SIZE]
+        batch_x = Variable(torch.Tensor(batch_x))
+        batch_y = train_y[ix: ix + BATCH_SIZE]
+        batch_y_onehot = get_onehot(batch_y)
+        batch_y = Variable(torch.LongTensor(batch_y))
+        batch_y_onehot = Variable(torch.FloatTensor(batch_y_onehot))
+        digicaps = model(batch_x.view(-1, 28, 28).unsqueeze(1))
+        reconstruction = model.reconstruct(digicaps, batch_y)
+        mloss = margin_loss(digicaps, batch_y_onehot)
+        rloss = reconstruction_loss(reconstruction, batch_x)
+        loss = mloss + 0.0005 * rloss
+        loss.backward()
+        opt.step()
+        opt.zero_grad()
+        print loss.data.numpy()[0]
+    # ==== Do validation stuff ============ #
